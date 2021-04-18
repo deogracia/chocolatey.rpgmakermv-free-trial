9683bd7dc71fc8ded0c8232b9f6f021d8d06608d
Lionel Félicité <deogracia@free.fr>
2021-04-18 20:19:38 +0200

    Take reviewer advices in

--- Releases_Notes.md
+++ Releases_Notes.md
@@ -4,6 +4,14 @@
 
 # Published
 
+## 1.6.1-2
+
+* Take review by TheCakeIsNaOH in ^^
+* Use Install-ChocolateyZipPackage instead of Get-ChocolateyWebFile and Get-ChocolateyUnzip
+* Add `trial` tag
+* Remove all comments
+* add in nuspec file bugTrackerUrl, licenseUrl & docsUrl tag
+
 ## 1.6.1-1
 
 * First try on chocolatey package
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,26 +1,25 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $artefact_name     = 'RPGMV_161W_Setup'
-$url               = "https://dl.degica.com/rpgmakerweb/trial-download/$artefact_name.zip" # download url, HTTPS preferred
+$url               = "https://dl.degica.com/rpgmakerweb/trial-download/$artefact_name.zip"
 $url_checksum      = 'BA49006D64E05A69ED9681380F6A544ED01BDD9AE348C3663FE5423C38D31855'
 $url_checksum_type = 'sha256'
 $my_temp_dir       = "$env:temp\$env:ChocolateyPackageName"
 
-Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath "$my_temp_dir\$env:ChocolateyPackageName.zip" -Url $url -Checksum $url_checksum -ChecksumType $url_checksum_type
-Get-ChocolateyUnzip   -PackageName $env:ChocolateyPackageName -FileFullPath "$my_temp_dir\$env:ChocolateyPackageName.zip" -Destination $my_temp_dir
+#Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath "$my_temp_dir\$env:ChocolateyPackageName.zip" -Url $url -Checksum $url_checksum -ChecksumType $url_checksum_type
+#Get-ChocolateyUnzip   -PackageName $env:ChocolateyPackageName -FileFullPath "$my_temp_dir\$env:ChocolateyPackageName.zip" -Destination $my_temp_dir
 
+Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -UnzipLocation "$my_temp_dir" -Url $url -Checksum $url_checksum -ChecksumType $url_checksum_type
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  url           = $url
-  fileType      = 'exe'
-  file          = "$my_temp_dir\$artefact_name\setup.exe"
-
-  softwareName  = 'rpgmakermv-free-trial*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  validExitCodes= @(0, 3010, 1641) #please insert other valid exit codes here
+  packageName    = $env:ChocolateyPackageName
+  url            = $url
+  fileType       = 'exe'
+  file           = "$my_temp_dir\$artefact_name\setup.exe"
+  softwareName   = 'rpgmakermv-free-trial*'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
